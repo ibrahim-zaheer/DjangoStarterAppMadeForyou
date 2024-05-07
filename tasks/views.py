ba27ea92bdd63634ceb808effc01ff4a869a23df
Ibrahim-Za <fa21-bcs-009@cuilahore.edu.pk>
2024-05-07 20:54:40 +0500

    changes are finished ready for use

--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,41 @@
-# from django.shortcuts import render, redirect
-# from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login
 # #if not want to use forms.py and just this code and register.html for making users then this is enough
-# def register_view(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
             
-#             form.save()
-#             return redirect('login')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'register.html', {'form': form})
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to the home page after successful login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+#now make this code when you want to redirect the user after login
+def home(request):
+    return render(request,"home.html")
 
 # #if want to use forms.py and set some default functions like staff status on and so on then use this:
 # from django.shortcuts import render, redirect
@@ -28,15 +53,15 @@
 
 
 #if want to use forms.py and set some default functions like staff status on and also set it to a certain group and so on then use this:
-from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+# from django.shortcuts import render, redirect
+# from .forms import CustomUserCreationForm
 
-def register_view(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'register.html', {'form': form})
+# def register_view(request):
+#     if request.method == 'POST':
+#         form = CustomUserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('login')
+#     else:
+#         form = CustomUserCreationForm()
+#     return render(request, 'register.html', {'form': form})
